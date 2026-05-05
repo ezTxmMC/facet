163f9ae6d881b5bba079c769cf5935608d9a8e38
ezTxmMC <tom.handrick@vesperia.net>
2026-05-05 05:50:05 +0200

    fixed building

--- src/facet.cr
+++ src/facet.cr
@@ -12,3 +12,18 @@ require "./facet/renderer"
 require "./facet/router"
 require "./facet/spa_router"
 require "./facet/server"
+
+module Facet
+  def self.build(router : Router, port : Int32 = 3000) : Server
+    Server.new(router, port)
+  end
+
+  def self.run(router : Router, port : Int32 = 3000) : Nil
+    server = build(router, port)
+    server.start
+  end
+
+  def self.compile_static(input_dir : String = ".", output_dir : String = "dist") : Nil
+    StaticCompiler.new(input_dir, output_dir).compile
+  end
+end
